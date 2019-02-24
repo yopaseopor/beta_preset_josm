c41310f7c46eebf4293db1df6c5f801c1c98fd46
yopaseopor <yopaseopor@gmail.com>
2019-02-24 19:16:51 +0100

    other out of presets

--- _other/_2/TH/Road signs in Thailand - Wikipedia_files/load.css
+++
@@ -1,1 +0,0 @@
-.anonymous-show{display:none !important}div.user-show,p.user-show{display:block !important}span.user-show,small.user-show{display:inline !important}table.user-show{display:table !important}li.user-show{display:list-item !important}
